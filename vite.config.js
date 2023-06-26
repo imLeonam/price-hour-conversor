2bc08d97b3b3a8a5fd18213295cf9eedc7d19d44
imLeonam <leonammrib@gmail.com>
2023-06-26 10:42:49 -0300

    feat(app): added vite to project

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // project related config
+  plugins: [
+    // plugins
+  ],
+  server: {
+    host: true,
+    port: 3000, // change port number if you prefer
+  },
+});
